4bd86e6793bc135c87d10042f8891fd5dbb00e02
Kumaraswamy121 <95460245+Kumaraswamy12@users.noreply.github.com>
2022-05-18 14:43:42 +0000

    ..

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -1,8 +1,8 @@
 def add(a, b):
     return a+b
 def main():
-    a = int(input('ENTER the value of a:'))
-    b = int(input('ENTER the value of b:'))
+    a = int(input('Enter 1st Number?'))
+    b = int(input('Enter 2nd Number?'))
     c = add(a, b)
-    print(f'The Sum of {a}+{b}={c}')
+    print(f'Sum of {a} and {b} is {c}')
 main()
