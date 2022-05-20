3ebb8ab9b6a68b6fa0681ad450c8858e04dcf43b
Kumaraswamy121 <95460245+Kumaraswamy12@users.noreply.github.com>
2022-05-20 13:20:01 +0000

    ..

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -6,9 +6,7 @@ def output(a, b, sum):
   print(f'{a}+{b} is {sum}')
   pass
 def main():
-  a= int(input('input?'))
-  b=int(input())
-  sum = add(a, b)
+  a,b= [int(input('INPUT?')),int(input())]
+  sum = add(a,b)
   output(a, b, sum)
-
 main()
