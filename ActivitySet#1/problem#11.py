f0c60de9954f263686e207a4f0ef814e25b12b56
Kumaraswamy121 <95460245+Kumaraswamy12@users.noreply.github.com>
2022-06-06 13:36:10 +0000

    ...

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,2 +1,20 @@
-# Tuples
-filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+if len(name) < 1:
+     name = "mbox-short.txt"
+handle = open(name)
+d={}
+t=[]
+for line in handle:
+    if line.startswith('From:'): continue
+    if not line.startswith('From'): continue
+    index=line.find(':')
+    st=line[index-2:index]
+    if st not in d:
+        d[st]=1
+    else:
+        d[st]+=1
+
+t = list(d.items())
+t.sort()
+for a,b in t:
+    print(a,b)
