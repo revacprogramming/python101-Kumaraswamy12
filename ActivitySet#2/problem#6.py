58377ba03ab11ce295c73e6cd8c9b319b4741cbc
Kumaraswamy121 <95460245+Kumaraswamy12@users.noreply.github.com>
2022-06-11 16:21:36 +0000

    ..

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -5,9 +5,7 @@ class Menu:
       def add(x,food,price):
            x.food=food
            x.price=price
-        
-
-
+      
 m = Menu()  # Menu is a class
 m.show(add("idly", 10))
 m.add("vada", 20)
