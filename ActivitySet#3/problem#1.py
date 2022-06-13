038256eaf89105795b7f4303df50d1b1d12d2a19
Kumaraswamy121 <95460245+Kumaraswamy12@users.noreply.github.com>
2022-06-13 05:07:05 +0000

    ..

---
+++ ActivitySet#3/problem#1.py
@@ -0,0 +1,12 @@
+import math
+def output():
+  x1,y1,x2,y2,x3,y3=map(float,input().split())
+  area=(math.sqrt((x3-x2)**2+(y3-y2)**2))*(math.sqrt((x2-x1)**2+(y2-y1)**2))
+  print(f"Area of rectangle with vertices ({x1},{y1}),({x2},{y2}),({x3},{y3}) is {area}" )
+def main():
+  a=int(input("enter the number of rectangles:"))
+  for i in range(a):
+      output()
+
+main()
+    
