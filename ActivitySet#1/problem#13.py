2fe89a7244f9d1f13b08a2766d258ec6cd6d3279
Kumaraswamy121 <95460245+Kumaraswamy12@users.noreply.github.com>
2022-06-27 03:16:51 +0000

    ..

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,2 +1,12 @@
 # Network Programming
 # https://www.py4e.com/lessons/network
+from urllib.request import*
+from bs4 import BeautifulSoup
+count=0
+url=input('enter the url :')
+html=urlopen(url).read()
+soup=BeautifulSoup(html,'html.parser')
+tags=soup('span')
+for tag in tags:
+   count+=int(tag.contents[0])
+print(count)
