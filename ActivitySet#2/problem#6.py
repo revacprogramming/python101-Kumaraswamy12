f0997f1f3f9ebfbc421e776752359fd73e071776
Kumaraswamy121 <95460245+Kumaraswamy12@users.noreply.github.com>
2022-07-07 16:01:25 +0000

    ..

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -1,15 +1,17 @@
 
 class Menu:
-      def show(x):
-        print(x.food,x.price)
-      def add(x,food,price):
+  def add(x,food,price):
            x.food=food
            x.price=price
+  def show(x):
+        print(x.food,x.price)
+      
 
 
   
-m = Menu()  # Menu is a class
-m.show(add("idly", 10))
+m = Menu()
+m.add("idly", 10)# Menu is a class
+m.show()
 m.add("vada", 20)
 m.show()
 
--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -1,10 +1,11 @@
 
 
 class Menu:
-    """fill in class definition here"""
+   # """fill in class definition here"""
 
+     def Menu():
+        m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)    
+        print(m)  # should print the menu properly
+        m = Menu()
+        
 
-m = Menu()
-m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)
-
-print(m)  # should print the menu properly
