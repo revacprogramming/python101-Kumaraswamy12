da06dacb7bbe655ecb66526ac1857f83bb0c4761
Kumaraswamy121 <95460245+Kumaraswamy12@users.noreply.github.com>
2022-07-20 06:44:03 +0000

    ..

--- ActivitySet#3/problem#2.py
+++ ActivitySet#3/problem#2.py
@@ -6,14 +6,16 @@ di={}
 g=0
 for i in range(x):
   y=int(input())
-  l=input().split
+  l=input().split()
+  g=0
+  ll=[]
   for j in range(y):
     g+=fc(f'1/{l[j]}')
     ll.append(fc(f'1/{l[j]}'))
   di[g]=ll
 for i,j in di.items():
-  print(*j,sep='+',end="=")
-  print(i)
+   print(*j,sep='+',end="=")
+   print(i)
   
  
-    
+  
