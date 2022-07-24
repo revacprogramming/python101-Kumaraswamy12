c1da485a3257345fef55ae1fa569e3f2ad635a16
Kumaraswamy121 <95460245+Kumaraswamy12@users.noreply.github.com>
2022-07-24 03:33:15 +0000

    ..

--- ActivitySet#1/problem#22.py
+++ ActivitySet#1/problem#22.py
@@ -1,40 +1,10 @@
-import random
-option = ["Rock", "Paper", "Scissors"]
-computer = random.choice(option)
-player = False
-cpu_score = 0
-player_score = 0
-tie=0
-while True:
-    player = input("Rock, Paper or  Scissors?").capitalize()
-    ## Conditions of Rock,Paper and Scissors starts from 11
-    if player == computer:
-        print("Tie!")
-        tie+=1
-    elif player == "Rock":
-        if computer == "Paper":
-            print("You lose!", computer, "covers", player)
-            cpu_score+=1
-        else:
-            print("You win!", player, "smashes", computer)
-            player_score+=1
-    elif player == "Paper":
-        if computer == "Scissors":
-            print("You lose!", computer, "cut", player)
-            cpu_score+=1
-        else:
-            print("You win!", player, "covers", computer)
-            player_score+=1
-    elif player == "Scissors":
-        if computer == "Rock":
-            print("You lose...", computer, "smashes", player)
-            cpu_score+=1
-        else:
-            print("You win!", player, "cut", computer)
-            player_score+=1
-    elif player not in option:
-        print("Final Scores:")
-        print(f"CPU:{cpu_score}")
-        print(f"Player:{player_score}")
-        print(f"TIE:{tie}")
-        break
+'''02''' 
+ from urllib.request import urlopen 
+ import json 
+ url='http://py4e-data.dr-chuck.net/comments_1550211.json' 
+ jsFile = urlopen(url) 
+ data =json.loads(jsFile.read()) 
+ sum=0 
+ for i in data['comments']: 
+     sum+=i['count'] 
+ print(sum)
